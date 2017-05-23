2dfe252e8b626ea257e7a8505788d34cfa367e7e
Michael Axelson <axelmichael11@gmail.com>
2017-05-23 03:12:06 -0700

    adding server, not working right now, in the process of rearranging files

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
+// 'use strict';
+//
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const PORT = process.env.PORT || 3000;
+//
+//
+//
+// const app = express();
+//
+// //This is the middleware of the code... From stackOverFlow.com it states that bodyparser.json(), "Parses the text as JSON and exposes the resulting object on req.body"
+// // app.use(bodyParser.json());
+// // app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static('.'));
+//
+// // So this connects the client to the Client Constructor that passes through the URL as the argument...
+// const client = new pg.Client(conString);
+//
+//
+// //This actually tells our port to start running when nodemon or node server.js is ran
+// app.listen(PORT, function() {
+//   console.log(`Server started on port ${PORT}!`);
+// });
+
+
+'use strict';
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3000;
-const fs = require('fs');
-
-
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-//This is the middleware of the code... From stackOverFlow.com it states that bodyparser.json(), "Parses the text as JSON and exposes the resulting object on req.body"
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('./public'));
+app.use(express.static('.'))
 
-// So this connects the client to the Client Constructor that passes through the URL as the argument...
-const client = new pg.Client(conString);
-
-
-//This actually tells our port to start running when nodemon or node server.js is ran
 app.listen(PORT, function() {
-  console.log(`Server started on port ${PORT}!`);
+
+  console.log('My Portfolio is open on port: ' + PORT);
 });
