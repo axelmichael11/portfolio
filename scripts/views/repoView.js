f9de0ae65510d7b676d8105ce6fe5f8ff36328f7
Michael Axelson <axelmichael11@gmail.com>
2017-05-28 20:32:58 -0700

    13 branch is not throwing errores at least, just trying to make an ajax request

--- scripts/views/repoView.js
+++ scripts/views/repoView.js
@@ -4,11 +4,11 @@ var app = app || {};
 (function(module) {
   const repoView = {};
 
-  let render = Handlebars.compile($('#repo-template').html());
+  const render = Handlebars.compile($('#repo-template').text());
 
   repoView.index = function() {
     $('#github').append(
-      app.repos.map(render) // Want to filter by a different property other than name?
+      app.repos.with('name').map(render) // Want to filter by a different property other than name?
     );
   };
   module.repoView = repoView;
